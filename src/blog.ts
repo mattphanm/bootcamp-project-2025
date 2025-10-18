2aac6a2cf8acec4d364cbd49610938d134259a61
MatthewPhan <Matthewminhphan@gmail.com>
2025-10-17 19:44:21 -0700

    Finish CSS + Layout

--- src/blog.ts
+++ src/blog.ts
@@ -2,53 +2,94 @@ type blog = {
 	title: string;
 	date: string;
 	description: string;
-	image: string;
-	imageAlt: string;
+	image?: string;
+	imageAlt?: string;
 	slug: string;
+  	video?: string;	
 };
 
 const blogs: blog[] = [
-	{ title: "Matcha", 
+	{ 
+	  title: "Matcha", 
 	  date: "Winter, 2024", 
 	  description: "Here is my attempt to recreate the Matcha Einspänners I have tried at the cafes I have visited in Los Angeles! After several attempts over the year, I finally found my favorite ratio for a homemade Matcha Einspänner. Through many trial and errors and experimenting with different oatmilk brands and sweetening syrups, I found the perfect balance of sweetness and umaminess.", 
 	  image: "../images/matcha.jpg", 
- 	  imageAlt: "A", 
+ 	  imageAlt: "matcha", 
  	  slug: "post-a" },
-	{ title: "cow", 
-          date: "Oct 1, 2025", 
-          description: "Intro...",
-          image: "./images/a.png",
-          imageAlt: "A", 
+	{ 
+	  title: "Intra-Day Trading Futures", 
+          date: "January 23, 2025", 
+          description: "My most valuable asset and passion of mines for the past 3 years have been Intraday Trading. A love and hate relationship. Through years of practice, patience, and discipline, I have progressed to the point where I am now...",
+          image: "./images/certificate.png",
+          imageAlt: "cert", 
           slug: "post-a" },
-
+	{ 
+	  title: "Golf",
+	  date: "Summer, 2025",
+	  description: "Here is a video of me trying out golfing over Summer. I have been going to the golf range with my friends, practicing my swings. I have not been consistent with this though. Playing golf with my friends at the nearest range, to me, is a good break away and a good bonding time...",
+	  video: "../images/golf.mp4",
+	  slug: "post-a",
+	}
 
 ];
 
 function blogsRender() {
 	const blogContainer = document.getElementById("blog-container");
-	if (!blogContainer) return console.warn("none"); 
+	if (!blogContainer) return; 
 	
-	blogs.forEach((blog) => {
-		const image = document.createElement("img");
-		image.src = blog.image;
-		image.alt = blog.imageAlt;
+	 
+	
+	blogs.forEach((blog, index) => {
 		
+	let media: HTMLImageElement | HTMLVideoElement;
+		
+        if (blog.video){
+                const video = document.createElement("video");
+                video.src = blog.video;
+                video.controls = true;
+                video.className = "blog-video";
+                media = video;
+        }
+        else if (blog.image) {
+                const image = document.createElement("img");
+                image.src = blog.image;
+                image.alt = blog.imageAlt;
+                image.className = "blog-image";
+                media = image;
+        }
+
+				
 		const div = document.createElement("div");
 		div.className = "blog-box";
 		
+		if (index % 2 === 1) {
+    			div.classList.add("reverse");
+  		}
+
 		const title = document.createElement("h2");
 		title.textContent = blog.title;
-		
+	 	title.className = "blog-title";
+			
 		const date = document.createElement("time");
 		date.textContent = blog.date;
+		date.className = "blog-date";
 	
 		const description = document.createElement("p");
 		description.textContent = blog.description;
+		description.className = "blog-description";
+					
 
-		div.append(image, title, date, description);	
+		const contentdiv = document.createElement("div");
+		contentdiv.className = "blog-content";		
+
+		div.append(media, contentdiv);	
 		if(blogContainer) {
 			blogContainer.append(div);
-		}
+	        
+  		contentdiv.append(title, date, description);	
+		
+				
+ 		}
 	});
 }
 
